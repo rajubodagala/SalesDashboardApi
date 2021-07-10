f1cc43ac5fc2da88c78bcf9e022312d3bd106514
rajubodagala <86926031+rajubodagala@users.noreply.github.com>
2021-07-10 08:13:08 +0530

    access control

--- App.js
+++ App.js
@@ -26,6 +26,17 @@ var authRouter = require('./routes/auth');
 //     }
 // }
 
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept,x-auth-token,x-refresh-token"
+    );
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  
+    next();
+  });
+
 
 app.use(bodyParser.json())
 app.use(express.urlencoded({ extended: false }));
