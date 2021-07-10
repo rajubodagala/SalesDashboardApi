09c7ec089a7f0fe7db9d28db8edcb9a85d576549
rajubodagala <86926031+rajubodagala@users.noreply.github.com>
2021-07-10 10:25:30 +0530

    port

--- App.js
+++ App.js
@@ -27,6 +27,16 @@ var authRouter = require('./routes/auth');
 // }
 
 app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept,x-auth-token,x-refresh-token"
+    );
+    next();
+  });
+app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header(
       "Access-Control-Allow-Headers",
